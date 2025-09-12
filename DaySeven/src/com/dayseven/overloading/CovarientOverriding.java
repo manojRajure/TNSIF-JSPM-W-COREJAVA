c7fc97af52ef25c4d5b0f7028a8afd3d4fb29b42
rajur <rajur@Manoj>
2025-09-12 10:56:17 +0530

    daySeven

---
+++ DaySeven/src/com/dayseven/overloading/CovarientOverriding.java
@@ -0,0 +1,29 @@
+package com.dayseven.overloading;
+
+class Color{
+	protected Color getColor() {
+		Color s = new Color();
+		return s;
+	}
+}
+
+class Yellow extends Color{
+	protected Yellow getColor() {
+		Yellow s = new Yellow();
+		return s;
+	}
+	
+}
+
+public class CovarientOverriding {
+
+	public static void main(String[] args) {
+		Color c= new Color();
+		System.out.println(c.getColor());
+		
+		Yellow y=new Yellow();
+		System.out.println(y.getColor());
+
+	}
+
+}
--- DaySeven/src/com/dayseven/overriding/OverridingDemo.java
+++ DaySeven/src/com/dayseven/overriding/OverridingDemo.java
@@ -12,8 +12,6 @@ public class OverridingDemo {
 		rbi=new RBI();
 		System.out.println(rbi.getRateofInterest());
 		
-		
-		
 	}
 
 }
